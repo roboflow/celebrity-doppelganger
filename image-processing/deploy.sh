f2d483023db08114df559fedefa7820ed9e11c9c
Brad Dwyer <brad@roboflow.com>
2023-04-26 10:22:44 -0500

    Fix deploy.sh

--- image-processing/deploy.sh
+++ image-processing/deploy.sh
@@ -1,2 +1,2 @@
-gcloud builds submit --tag gcr.io/rf-doppelganger/doppelganger-python --project rf-magicscissors
+gcloud builds submit --tag gcr.io/rf-doppelganger/doppelganger-python --project rf-doppelganger
 gcloud run deploy doppelganger-python --image gcr.io/rf-doppelganger/doppelganger-python --platform managed --region us-central1 --allow-unauthenticated --project rf-doppelganger --memory 16G --cpu 8 --timeout 60m --concurrency 1 --execution-environment gen2
